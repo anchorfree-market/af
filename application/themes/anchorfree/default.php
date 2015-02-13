fb3dfc955f970845f19cb36be8033568a48d1253
Esther Lau <esther@anchorfree.com>
2015-02-12 17:52:43 -0800

    added default.php

---
+++ application/themes/anchorfree/default.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>VPN Service: Download Hotspot Shield Free & Elite VPN Client</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="/source/v.1.0/css/styles.min.css" />
+<!--[if lt IE 9]>
+<script src="/js/html5shiv.js"></script>
+<![endif]-->
+</head>
+<body class="af-body">
+  <div class="af-container">
+<div class="head-image"></div>
+<header class="head-body">
+      <a href="/" title="AnchorFree Hotspot Shield VPN"><img src="/source/v.1.0/images/logo_anchorfree.png" alt="anchorfree logo" class="logo"></a>
+      <ul class="menu nav">
+        <li><a href="http://www.hotspotshield.com">HOTSPOT SHIELD VPN</a></li>
+        <li><a href="advertise.php"   title="Advertise">Advertise</a></li>
+        <li><a href="carriers.php"   title="">Carriers</a></li>
+        <li><a href="news.php"   title="Hotspot Shield News">News</a></li>
+        <li><a href="about.php"   title="About Hotspot Shield">About</a></li>
+        <li><a href="https://hsselite.zendesk.com/home" title="Hotspot Shield Support">Support</a></li>
+        <li><a href="contact.php"   title="Contact Hotspot Shield">Contact</a></li>
+      </ul>
+		<script type="text/javascript">
+		    var redirectURLValue = '';
+		    function redirectURL(href) {
+		        redirectURLValue = href;
+		        setTimeout('window.location=redirectURLValue', 300);
+		    }
+		</script>
+      <h1>Unleash the Power of Hotspot Shield VPN<br>
+          <span>Protect Your Privacy. Secure Your Data. Unblock Websites.</span>
+      </h1>
+      <div class="available-platform">Available on <span></span></div>
+      <div class="af-btn-box">
+	      	      	      	      <a class="btn_download" href="macappstore://itunes.apple.com/app/id771076721?mt=12" onclick="_gaq.push(['_trackEvent', 'Homepage', 'freedownload_mac']); redirectURL(this.href); return false" title="AnchorFree Hotspot Shield VPN For MAC">Free Download</a>
+      </div>
+      <a class="btn_download_elite" href="http://www.hsselite.com/buy?af_source=hs_hss1_home" onclick="_gaq.push(['_trackEvent', 'hss1', 'buy now']); redirectURL(this.href); return false">Buy The Elite Version</a>
+	</header><!-- end .head-body -->
+    <div class="box-as-seen-in"><h2><a href="/news.php">The Most Trusted VPN Service With More than 300 Million Downloads</a></h2>
+    	<div class="vpn-download-news"></div>
+    </div>
+  </div><!-- end .head-wrapper -->
+    <div class="af-container">
+      <div class="artic">
+<!-- page -->
+      <div class="af-intro-box af-gray">
+      	<div class="af-wrapper">
+	        <h2>Benefits and Advantages of Hotspot Shield VPN Service</h2>
+	        <a class="right" href="javascript:void(0);" onclick="video_play();"><img src="/source/v.1.0/images/callout-video.png" alt="" height="246" width="436" align="right" /></a>
+	        <p>Every time you surf the Web, you expose yourself and your devices to multiple online threats & risks; Hackers could steal your passwords & financial information and the sites you visit may contain malware.</p>
+	        <p>Hotspot Shield VPN software uses advanced encryption technology to secure your browsing session, detect and blocks malware, and enables you to access your favorite content from anywhere. You can even bypass geo-restrictions to <a href="http://www.hotspotshield.com/unblock-facebook">unblock Facebook</a>, <a href="http://www.hotspotshield.com/unblock-youtube">unblock Youtube</a>, or <a href="http://www.hotspotshield.com/unblock-sites">unblock any website</a> in countries that censor content.</p>
+	        <p>Our Hotspot Shield VPN service  also <a href="http://www.hotspotshield.com/hide-ip-address">protects your IP address</a>, enabling you to surf the Web anonymously and privately.</p>
+	        <p><a href="http://www.hotspotshield.com/benefits-of-vpn">Learn more about the benefits of Hotspot Shield VPN</a></p>
+	        <div id="popup_view"></div>
+        </div>
+      </div>
+      <div class="af-intro-box af-white">
+      	<div class="af-wrapper" style="height: 460px;">
+	        <h2>Available on Windows, Mac, iPhone/iPad & Android</h2>
+	        <img src="/source/v.1.0/images/callout-mobile.jpg" alt="" height="402" width="381" align="left" style="margin-right:30px" />
+	        <br /><br />
+	        <p>Hotspot Shield VPN works on Windows PC, Mac, iPhone, iPad, iPod Touch, and Android devices.</p>
+	        <p>All the great features of the desktop product, including <a href="http://www.hotspotshield.com/wifi-security">WiFi security</a> , <a href="http://www.hotspotshield.com/anonymous-web-surfing">anonymous surfing</a>, and <a href="http://www.hotspotshield.com/malware-protection">malware protection</a> are available for your mobile device as well.</p>
+	        <p>An added bonus for mobile users is the data compression feature, which helps you to save bandwidth and $$$ on your phone bill.</p>
+	        <br />
+	        <a target="_blank" href="https://play.google.com/store/apps/details?id=hotspotshield.android.vpn&feature=nav_result#?t=W251bGwsMSwxLDMsImhvdHNwb3RzaGllbGQuYW5kcm9pZC52cG4iXQ.."><img src="/source/v.1.0/images/btn_go_to_google_play@2x.png" border="0" width="130" height="44" /></a>
+	        &nbsp;&nbsp;
+	        <a target="_blank" href="https://itunes.apple.com/app/id443369807?&referrer=click%3D3842cd54-b48a-45e5-b443-5339973be7b1"><img src="/source/v.1.0/images/btn_go_to_app_store@2x.png" border="0" height="44" width="125" /></a>
+        </div>
+      </div>
+      <div class="af-intro-box af-gray">
+      	<div class="af-wrapper">
+	        <h2>Try the Elite Version and Get Enhanced Performance & Support</h2>
+	        <img src="/source/v.1.0/images/callout-elite@2x.png" alt="Upgrade to Hotspot Shield Elite" height="272" width="467" align="right" style="margin-left:35px" />
+	        <div class="align_right">
+	          <p>The standard Hotspot Shield version is a free, ad-supported VPN software. However, upgrading to Hotspot Shield Elite brings enhanced performance and support. The Hotspot Shield Elite <b>Virtual Private Network</b> comes with:</p>
+	          <ul>
+	          <li class="first">Faster Access</li>
+	          <li>24/7 Customer Support</li>
+	          <li><a href="http://www.hotspotshield.com/malware-protection">Advanced Malware Protection</a></li>
+	          <li class="last">No Ads!</li>
+	          </ul>
+	          <a class="btn-green" onclick="_gaq.push(['_trackEvent', 'Homepage', 'Findoutmore']);" href="https://www.hsselite.com/pricing??utm_medium=AFinternal&utm_source=AFcom&utm_campaign=HomepageBtm">Find Out More</a>
+	        </div>
+    	</div>
+      </div>
+
+    </div><!-- end .artic -->
+
+<!-- end:page -->
+<footer class="footer">
+        <div class="footer-wrapper">
+            <div class="dev_links">
+                <a href="https://play.google.com/store/apps/details?id=hotspotshield.android.vpn">Android<span style="background-position: -336px 0;"></span></a>
+                <a href="https://itunes.apple.com/us/app/hotspot-shield-vpn-for-iphone/id443369807">iPhone<span></span></a>
+                <a id="macVersion" href="http://download.cnet.com/Hotspot-Shield/3000-2092_4-10638005.html">Mac<span style="background-position: -381px 0;"></span></a>
+                <a href="http://download.cnet.com/hotspot-shield/">Windows<span style="background-position: -426px 0;"></span></a>
+            </div>
+            <div class="footer-nav">
+              <ul>
+                <li><a href="/jobs/" title="Jobs">We're Hiring!</a></li>
+                <li><a href="/privacy.php" title="Privacy">Privacy</a></li>
+                <li><a href="/terms-of-service.php" title="Terms of Service">Terms</a></li>
+                <li><a href="/support.php" title="Hotspot Shield Help">Help</a></li>
+                <li><a href="http://rpt.anchorfree.net/afrpt.php?afcid=1952&affr=click_link&tag=UNREGISTERED&cnl=empty&afhss=UNDEFINED&sip=1249051728&rdr=http%3A%2F%2Fanchorfree.us">Visit Us</a></li>
+              </ul>
+              <p class="copyright">Copyright &copy; 2015 AnchorFree, Inc. All Rights Reserved</p>
+          </div>
+      </div>
+      <div class="af-footer-bottom">
+        <div class="af-footer-column af-footer-red"></div>
+        <div class="af-footer-column af-footer-blue"></div>
+        <div class="af-footer-column af-footer-column-last af-footer-green"></div>
+      </div>
+</footer>
+    
+    </div>\n<!-- Google Code for Homepage Visitors Remarketing List -->
+<script type="text/javascript">
+  /* <![CDATA[ */
+  var google_conversion_id = 1067709949;
+  var google_conversion_language = "en";
+  var google_conversion_format = "3";
+  var google_conversion_color = "666666";
+  var google_conversion_label = "WbFECLuykQIQ_euP_QM";
+  var google_conversion_value = 0;
+  /* ]]> */
+</script>
+<script type="text/javascript" src="http://www.googleadservices.com/pagead/conversion.js"></script>
+<noscript>
+  <div style="display:inline;">
+    <img height="1" width="1" style="border-style:none;" alt="" src="http://www.googleadservices.com/pagead/conversion/1067709949/?label=WbFECLuykQIQ_euP_QM&amp;guid=ON&amp;script=0"/>
+  </div>
+</noscript>
+<iframe src='http://pixel.fetchback.com/serve/fb/pdj?cat=&name=landing&sid=4559' scrolling='no'	width='1' height='1' marginheight='0' marginwidth='0' frameborder='0'></iframe>
+<script type="text/javascript" language="javascript">llactid=22127</script>
+<script type="text/javascript">
+// display different download link for Mac button in footer based on Mac version 
+function OSVersion() {
+  if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1) { // firefox
+    var v = (navigator.userAgent).match(/OS X (\d+).(\d+).?(\d+)?/);
+    return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+  }
+  else { // other browsers
+    var v = (navigator.userAgent).match(/OS X (\d+)_(\d+)_?(\d+)?/);
+    return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+  }
+}
+var ver = OSVersion();
+var macVersionID = document.getElementById('macVersion');
+if(ver[1] >=7) {
+    macVersionID.setAttribute('href', 'https://itunes.apple.com/us/app/hotspot-shield-vpn-best-vpn/id771076721');
+}
+</script>
+</body>
+</html>
