d04f516fdc1711ca83483ec6bb0dd10299be4153
Esther Lau <esther@anchorfree.com>
2015-02-13 14:12:06 -0800

    edited full.php

---
+++ application/themes/af/full.php
@@ -0,0 +1,7 @@
+<?php $view->inc('elements/header.php');  ?>
+        <?
+            $a = new Area('Sticky Contact Us');
+            $a->display($c);
+        ?>
+
+ <?php $view->inc('elements/footer.php'); ?>
