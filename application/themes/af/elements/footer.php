1008fecc5f7beb8b81f8c953e203381d4b7e06fc
Esther Lau <esther@anchorfree.com>
2015-02-13 16:22:25 -0800

    added supported js files; added js links to footer.php

--- application/themes/af/elements/footer.php
+++ application/themes/af/elements/footer.php
@@ -3,11 +3,11 @@
                 <div class="row">
                     <div class="col-md-12">
                         <ul class="nav nav-justified footer-nav">
-                            <li><a title="Privacy" href="/privacy.php">Privacy</a></li>
-                            <li><a title="Terms" href="/terms-of-service.php">Terms</a></li>
-                            <li><a title="We’are hiring!" href="/jobs/">We’re hiring!</a></li>
+                            <li><a title="Privacy" href="/~anchorj6/af/privacy">Privacy</a></li>
+                            <li><a title="Terms" href="/~anchorj6/af/terms-of-service.php">Terms</a></li>
+                            <li><a title="We’are hiring!" href="/~anchorj6/af/jobs/">We’re hiring!</a></li>
                             <li><a title="Help" href="/support.php">Help</a></li>
-                            <li><a title="Visit us" href="/contact.php">Visit us</a></li>
+                            <li><a title="Visit us" href="/~anchorj6/af/contact">Visit us</a></li>
                         </ul>
                         <div class="separator saperator-gradient"></div>
                         <p class="footer-copyright">Copyright &copy; 2015 AnchorFree, Inc. All Rights Reserved</p>
@@ -19,12 +19,12 @@
 
 <!-- JavaScript 
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+-->
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/js/jasny-bootstrap.min.js"></script>
 <script type="text/javascript" src="//cdn.jsdelivr.net/countupjs/1.1.0/countUp.min.js"></script>
-<script type="text/javascript" src="/js/v2/imageLens.min.js"></script>
-<script type="text/javascript" src="/js/v2/main-ck.js"></script>
--->
+<script type="text/javascript" src="<?php echo $view->getThemePath()?>/js/imageLens.min.js"></script>
+<script type="text/javascript" src="<?php echo $view->getThemePath()?>/js/main-ck.js"></script>
     </div>
     <? Loader::element('footer_required')?>
 </body>
