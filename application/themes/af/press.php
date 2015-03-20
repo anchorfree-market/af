d832716da5665629c911471fb7e72fe16488bc70
Esther Lau <esther@anchorfree.com>
2015-03-20 15:18:41 -0700

    added new blue bg image to press release; edited css

--- application/themes/af/press.php
+++ application/themes/af/press.php
@@ -1,10 +1,11 @@
 <?php 
 $view->inc('elements/header.php');  ?>
-<h2><? $a = new Area('Press Top Text');$a->display($c); ?></h2>
-<h1>Press Releases</h1>
-<h1>
-    <? $a = new Area('Press Title');$a->display($c); ?>
-</h1>
-<div class="press-date"><? $a = new Area('Press Date');$a->display($c); ?></div>
-<div class="press-desc"><? $a = new Area('Press Description');$a->display($c); ?></div>
+<div class="wrapper mid press">
+    <h2 class="main-title">Press Releases</h2>
+    <h2>
+        <? $a = new Area('Press Title');$a->display($c); ?>
+    </h2>
+    <div class="press-date"><? $a = new Area('Press Date');$a->display($c); ?></div>
+    <div class="press-desc"><? $a = new Area('Press Description');$a->display($c); ?></div>
+</div>
 <?php $view->inc('elements/footer.php'); ?>
