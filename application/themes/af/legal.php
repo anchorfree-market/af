d6cf7b0689fb8b62f0090f73db08fe63d165d48c
Robert Evans <robert@tresnadesigns.com>
2015-05-26 14:27:09 -0700

    needs top spacing

--- application/themes/af/legal.php
+++ application/themes/af/legal.php
@@ -1,6 +1,6 @@
 <?php 
 $view->inc('elements/header.php');  ?>
-
+<? $a = new Area('Section Top Area');$a->display($c); ?>
     <? $a = new Area('Section Main Area');$a->display($c); ?>
 
  <?php $view->inc('elements/footer.php'); ?>
