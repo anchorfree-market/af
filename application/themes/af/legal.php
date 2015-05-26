bd26bab065a45e3b1c7fd6397acce3e442037fd5
Robert Evans <robert@tresnadesigns.com>
2015-05-26 14:37:49 -0700

    Added top margin class

--- application/themes/af/legal.php
+++ application/themes/af/legal.php
@@ -1,6 +1,7 @@
 <?php 
 $view->inc('elements/header.php');  ?>
-<? $a = new Area('Section Top Area');$a->display($c); ?>
+<section class="section section-about-us about-us top-section">
+</section>
     <? $a = new Area('Section Main Area');$a->display($c); ?>
 
  <?php $view->inc('elements/footer.php'); ?>
