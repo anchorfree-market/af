2df633ae2b14f8b51cb64a0f09f61ee8416f7ca7
Robert Evans <robert@tresnadesigns.com>
2015-05-26 16:54:21 -0700

    page complete redesign - test
    testing

--- application/themes/af/release.php
+++ application/themes/af/release.php
@@ -1,15 +1,19 @@
 <?php $view->inc('elements/header.php');  ?>
 
 <div class="container-fluid top-section">
-	<div class="row">
     <div class="col-md-12">
-    <?php $view->inc('elements/txt-left-img-right.php'); ?>
-    <? $a = new Area('Backstretch Script');$a->display($c); ?>
-    </div>
-    </div>
+    <? $a = new GlobalArea('Background Header Image');$a->display($c); ?>
+	<div class="wrapper txt-left backstretch-contain">
+    <div class="container">
+        <div class="col-sm-8 col-md-7 col-lg-6">
+            <? $a = new Area('Text Left');$a->display($c); ?>
+        </div>
+        <div class="clearfix"></div>
+    </div></div></div>
+</div>
+
 <div class="row">
-	<div class="container">
-<div class="wrapper mid press">
+<div class="container wrapper mid press">
     <div class="col-md-8">
         <h2 class="main-title"><? $a = new Area('Press Title');$a->display($c); ?></h2>
         <div class="press-date"><? $a = new GlobalArea('Press Release Lists');$a->display($c); ?></div>
@@ -18,7 +22,7 @@
         <? $a = new GlobalArea('Press Featured On');$a->display($c); ?>
     </div>
 </div>
-</div></div></div>
+</div>
     
 <?php $view->inc('elements/footer.php'); ?>
 
