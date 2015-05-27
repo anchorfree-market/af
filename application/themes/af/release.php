87fca5c57c3a160b33f74a51fc82ec85eb4c2f42
Robert Evans <robert@tresnadesigns.com>
2015-05-26 17:11:14 -0700

    release.php
    Checking with nested divs

--- application/themes/af/release.php
+++ application/themes/af/release.php
@@ -1,7 +1,23 @@
 <?php $view->inc('elements/header.php');  ?>
 
-<div class="container-fluid top-section txt-left">
+<div class="container-fluid top-section">
+<div class="row">
+  <div class="col-sm-12 txt-left">
     <? $a = new GlobalArea('Background Header Image');$a->display($c); ?>
+    <div class="row">
+      <div class="col-xs-6 col-sm-6">
+        <? $a = new Area('Text Left');$a->display($c); ?>
+      </div>
+      <div class="col-xs-6 col-sm-6">
+        extra nested space
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="container-fluid top-section txt-left">
+    <div class="row"><? $a = new GlobalArea('Background Header Image');$a->display($c); ?>
 	<div class="backstretch-contain">
     <div class="container">
         <div class="col-sm-8 col-md-7 col-lg-6">
