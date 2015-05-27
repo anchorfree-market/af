1b5766f7e071887ed9c18565afc5314b25b7e48d
Robert Evans <robert@tresnadesigns.com>
2015-05-26 20:42:07 -0700

    additional side navigation areas

--- application/themes/af/release.php
+++ application/themes/af/release.php
@@ -11,14 +11,18 @@
 <div class="container-fluid">
 <div class="row">
 <div class="container">
-	<div class="wrapper mid press">
-    <div class="col-md-9">
+    <div class="col-md-8 press">
         <div><h2 class="main-title"><? $a = new Area('Press Title');$a->display($c); ?></h2></div>
         <div class="press-date"><? $a = new GlobalArea('Press Release Lists');$a->display($c); ?></div>
     </div>
-    </div>
-    <div class="col-md-3 press-feature hidden-xs">
-        <? $a = new GlobalArea('Press Featured On');$a->display($c); ?>
+    <div class="col-md-3 col-md-offset-1 press-feature hidden-xs">
+        <div>
+        <h3><? $a = new GlobalArea('Press Inquiries Title');$a->display($c); ?></h3>
+        <p><? $a = new GlobalArea('Press Inquiries');$a->display($c); ?></p>
+        <p>&nbsp;</p>
+        <h3><? $a = new GlobalArea('Press Featured On');$a->display($c); ?></h3>
+        <p><? $a = new GlobalArea('Press Featured Logos');$a->display($c); ?></p>
+        </div>
     </div>
 </div>
 </div>
