f48a67780150572ea9546b9f9c09979546b8a23c
Robert Evans <robert@tresnadesigns.com>
2015-05-26 20:15:35 -0700

    footer fixes

--- application/themes/af/elements/footer.php
+++ application/themes/af/elements/footer.php
@@ -2,17 +2,19 @@
             <div class="container-fluid">
                 <div class="row">
                 <div class="container">
-                    <div class="col-md-3 col-sm-3"><? $a = new GlobalArea('Footer Navigation');$a->display($c); ?></div>
-                    <div class="col-md-3 col-sm-3"><? $a = new GlobalArea('Footer Navigation2');$a->display($c); ?></div>
+                    <div class="col-md-3 col-sm-3 nav footer-nav"><? $a = new GlobalArea('Footer Navigation');$a->display($c); ?></div>
+                    <div class="col-md-3 col-sm-3 nav footer-nav"><? $a = new GlobalArea('Footer Navigation2');$a->display($c); ?></div>
                     <div class="recent-news col-md-6 col-sm-6 hidden-xs"><? $a = new GlobalArea('Footer News');$a->display($c); ?></div>
                 </div>
                 
                     <div class="separator saperator-gradient"></div>
                        
                 <div class="row">
-                	<div class="footer-copyright col-md-8 col-sm-6 col-xs-6"><? $a = new GlobalArea('Copyright');$a->display($c); ?></div>
+                <div class="container">
+					<div class="footer-copyright col-md-8 col-sm-6 col-xs-6"><? $a = new GlobalArea('Copyright');$a->display($c); ?></div>
 					<div class="footer-copyright col-md-4 col-sm-6 col-xs-6"><? $a = new GlobalArea('Social Links');$a->display($c); ?></div>
 				</div>
+                </div>
                 </div> <!-- .row -->
             </div> <!-- .container -->
         </footer>
