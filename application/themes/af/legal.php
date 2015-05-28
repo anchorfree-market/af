9192e9d4def0d74d93c81bec464028ffda5a0c96
Esther Lau <esther@anchorfree.com>
2015-05-27 17:05:00 -0700

    changed new area in legal.php

--- application/themes/af/legal.php
+++ application/themes/af/legal.php
@@ -8,4 +8,4 @@ $view->inc('elements/header.php');  ?>
     <? $a = new Area('Section Main Area');$a->display($c); ?>
 
  <?php $view->inc('elements/footer.php'); ?>
-<? $a = new Area('Backstretch Script');$a->display($c); ?>
+<? $a = new Area('php script');$a->display($c); ?>
