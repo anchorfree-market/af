f2ad7862432767e0824ae557bb387a0aba115fe6
Esther Lau <esther@anchorfree.com>
2015-05-27 17:20:34 -0700

    added php redirect to legal.php for testing

--- application/themes/af/legal.php
+++ application/themes/af/legal.php
@@ -8,4 +8,12 @@ $view->inc('elements/header.php');  ?>
     <? $a = new Area('Section Main Area');$a->display($c); ?>
 
  <?php $view->inc('elements/footer.php'); ?>
+<?php
+if ($redirectURL = $c->getCollectionAttributeValue('redirect_to_url')) {
+   if ($redirectURL != '') {   
+      Header( "HTTP/1.1 301 Moved Permanently" ); 
+      Header( "Location: " . $redirectURL ); 
+   }
+}
+?>
 <? $a = new Area('php script');$a->display($c); ?>
