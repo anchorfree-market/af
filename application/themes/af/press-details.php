f057a7baf07827f4c716c848797480a74415f3ec
Robert Evans <robert@tresnadesigns.com>
2015-06-01 14:31:35 -0700

    detail changes for page details page

--- application/themes/af/press-details.php
+++ application/themes/af/press-details.php
@@ -13,11 +13,11 @@ $view->inc('elements/header.php');  ?>
 <div class="row">
     <div class="col-md-8">
     	<!--Get Page Name--><h2 class="align-left"><?php $page = Page::getCurrentPage(); echo $page->getCollectionName(); ?></h2>
-        <div><p class="text-center"><? $a = new Area('Press Intro');$a->display($c); ?></p></div>
-        <div><h5 class="align-left">
+        <div><h4 class="text-center lead"><em><? $a = new Area('Press Intro');$a->display($c); ?></em></h4></div>
+        <div><p class="align-left">
 		<?php $datePublic = date('F j, Y', strtotime($page->getCollectionDatePublic())); echo $datePublic ?> &mdash; 
-		<?php $page = Page::getCurrentPage(); echo $page->getCollectionAttributeValue('press_detail_city'); ?>)</h5>
-		<p class="align-left"><? $a = new Area('Press Description');$a->display($c); ?></p></div>
+		<?php $page = Page::getCurrentPage(); echo $page->getCollectionAttributeValue('press_detail_city'); ?><br>
+		<? $a = new Area('Press Description');$a->display($c); ?></p></div>
     </div>
     <div class="col-md-3 col-md-offset-1 press-feature hidden-xs">
         <div>
