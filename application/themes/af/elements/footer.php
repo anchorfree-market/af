f06e58d2636830f60e604e0a0e0af78dbdf1bcee
Robert Evans <robert@tresnadesigns.com>
2015-06-01 15:59:33 -0700

    simplification of footer

--- application/themes/af/elements/footer.php
+++ application/themes/af/elements/footer.php
@@ -5,23 +5,24 @@
                 <div class="separator saperator-gradient"></div>
                 
                 <div class="container">
+                    <div class="row">
+                    <div class="col-md-12"><? $a = new GlobalArea('Full Width Area');$a->display($c); ?></div>
                     <div class="col-md-3 col-sm-3 col-xs-6 nav footer-nav"><? $a = new GlobalArea('Footer Navigation');$a->display($c); ?></div>
                     <div class="col-md-3 col-sm-3 col-xs-6 nav footer-nav"><? $a = new GlobalArea('Footer Navigation2');$a->display($c); ?></div>
                     <div class="recent-news col-md-6 col-sm-6 hidden-xs"><? $a = new GlobalArea('Footer News');$a->display($c); ?></div>
+                    </div>
+                    
+                    <div class="row">
+                    <div class="col-md-12"><? $a = new GlobalArea('Full Width Area');$a->display($c); ?></div>
+                    </div>
+                    
+                    <div class="row">
+                    <div class="footer-copyright col-md-8 col-sm-6 col-xs-8"><? $a = new GlobalArea('Copyright');$a->display($c); ?></div>
+					<div class="col-md-4 col-sm-6 col-xs-3 pull-right"><? $a = new GlobalArea('Social Links');$a->display($c); ?></div>
+                    </div>
                 </div>
                        
-                <div class="row">
-                <div class="container">
-					<div class="col-md-12"><? $a = new GlobalArea('Full Width Area');$a->display($c); ?></div>
-                </div>
-                </div>
-                    
-                <div class="row">
-                <div class="container">
-                    <div class="footer-copyright col-md-8 col-sm-6 col-xs-8"><? $a = new GlobalArea('Copyright');$a->display($c); ?></div>
-					<div class="col-md-4 col-sm-6 col-xs-3"><? $a = new GlobalArea('Social Links');$a->display($c); ?></div>
-				</div>
-                </div>
+                
                 </div> <!-- .row -->
             </div> <!-- .container -->
         </footer>
