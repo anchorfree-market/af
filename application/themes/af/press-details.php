dbff3a3c1c6d3fd33694f67d52a692c2b91d46c3
Robert Evans <robert@tresnadesigns.com>
2015-06-01 14:21:48 -0700

    code change

--- application/themes/af/press-details.php
+++ application/themes/af/press-details.php
@@ -16,7 +16,7 @@ $view->inc('elements/header.php');  ?>
         <div><p class="text-center"><? $a = new Area('Press Intro');$a->display($c); ?></p></div>
         <div><h5 class="align-left">
 		<?php $datePublic = date('F j, Y', strtotime($page->getCollectionDatePublic())); echo $datePublic ?> &mdash; 
-		<?php $page = Page::getCollectionAttributeValue('press_detail_city'); echo $page->getCollectionAttributeValue(); ?>)</h5>
+		<?php $page = Page::getCollectionAttributeValue(); echo $page->getCollectionAttributeValue('press_detail_city'); ?>)</h5>
 		<p class="align-left"><? $a = new Area('Press Description');$a->display($c); ?></p></div>
     </div>
     <div class="col-md-3 col-md-offset-1 press-feature hidden-xs">
