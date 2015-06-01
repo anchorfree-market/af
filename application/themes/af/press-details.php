8c5f6d9b287cfa2f583fa3b059b7803b7b7a4b8b
Robert Evans <robert@tresnadesigns.com>
2015-06-01 12:18:02 -0700

    details structure
    changing the overall structure to include top press name

--- application/themes/af/press-details.php
+++ application/themes/af/press-details.php
@@ -1,21 +1,33 @@
 <?php 
 $view->inc('elements/header.php');  ?>
 <div class="container-fluid top-section">
-<div class="row">
-	<div class="container">
+    <div class="backstretch-contain-press">
+        <div class="container">
+            <h1><? $a = new GlobalArea('Press Title Name');$a->display($c); ?></h1>
+        </div>
+    </div>
+    
+    <div class="row">
+<div class="container">
 <div class="wrapper mid press">
     <div class="col-md-8">
-        <h2>
-            <? $a = new Area('Press Title');$a->display($c); ?>
-        </h2>
-        <div class="press-date"><? $a = new Area('Press Date');$a->display($c); ?></div>
+        <h2><? $a = new Area('Press Title');$a->display($c); ?></h2>
+        <div class="press-date"><? $a = new Area('Press Date');$a->display($c); ?><p>&nbsp;</p></div>
         <div class="press-desc"><? $a = new Area('Press Description');$a->display($c); ?></div>
     </div>
-    <div class="col-md-4 press-feature hidden-xs">
-        <? $a = new GlobalArea('Press Featured On');$a->display($c); ?>
+    <div class="col-md-3 col-md-offset-1 press-feature hidden-xs">
+        <div>
+        <h3><? $a = new GlobalArea('Press Inquiries Title');$a->display($c); ?></h3>
+        <p><? $a = new GlobalArea('Press Inquiries');$a->display($c); ?></p>
+        <p>&nbsp;</p>
+        <h3><? $a = new GlobalArea('Press Featured On');$a->display($c); ?></h3>
+        <p><? $a = new GlobalArea('Press Featured Logos');$a->display($c); ?></p>
+        </div>
     </div>
 </div>
-</div></div></div>
+</div>
+</div>
+</div>
 <?php
 if ($redirectURL = $c->getCollectionAttributeValue('redirect_to_url')) {
    if ($redirectURL != '') {   
