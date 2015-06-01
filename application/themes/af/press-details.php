990d031a3c341e14ae7795b1ac99ea218420b861
Robert Evans <robert@tresnadesigns.com>
2015-06-01 14:36:25 -0700

    addl change

--- application/themes/af/press-details.php
+++ application/themes/af/press-details.php
@@ -14,8 +14,8 @@ $view->inc('elements/header.php');  ?>
     	<!--Get Page Name--><h2 class="align-left"><?php $page = Page::getCurrentPage(); echo $page->getCollectionName(); ?></h2>
         <div><h4 class="text-center lead"><em><? $a = new Area('Press Intro');$a->display($c); ?></em></h4></div>
         <div><p class="align-left">
-		<?php $datePublic = date('F j, Y', strtotime($page->getCollectionDatePublic())); echo $datePublic ?> &mdash; 
-		<?php $page = Page::getCurrentPage(); echo $page->getCollectionAttributeValue('press_detail_city'); ?><br>
+		<strong><?php $datePublic = date('F j, Y', strtotime($page->getCollectionDatePublic())); echo $datePublic ?> &mdash; 
+		<?php $page = Page::getCurrentPage(); echo $page->getCollectionAttributeValue('press_detail_city'); ?></strong><br>
 		<? $a = new Area('Press Description');$a->display($c); ?></p></div>
     </div>
     <div class="col-md-3 col-md-offset-1 press-feature hidden-xs">
