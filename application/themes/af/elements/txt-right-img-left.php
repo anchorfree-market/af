07057f7f87a60014bdc66b44cd2f758d082597e5
Robert Evans <robert@tresnadesigns.com>
2015-06-02 13:15:06 -0700

    adding css gradient

--- application/themes/af/elements/txt-right-img-left.php
+++ application/themes/af/elements/txt-right-img-left.php
@@ -1,4 +1,4 @@
-<div class="txt-right backstretch-contain">
+<div class="rightfadeblue backstretch-contain">
     <div class="container">
         <div class="col-md-6 align-left"><? $a = new Area('Text Left');$a->display($c); ?></div>
         <div class="col-md-6 align-right"><? $a = new Area('Text Right');$a->display($c); ?></div>
