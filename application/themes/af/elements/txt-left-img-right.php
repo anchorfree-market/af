bc75c18898beba62e4289e94598853daad16ef4c
Robert Evans <robert@tresnadesigns.com>
2015-06-01 17:15:08 -0700

    additional changes
    enlarging press featured text area

--- application/themes/af/elements/txt-left-img-right.php
+++ application/themes/af/elements/txt-left-img-right.php
@@ -1,6 +1,6 @@
 <div class="backstretch-contain">
     <div class="container">
-        <div class="col-sm-8 col-md-7 col-lg-6">
+        <div class="col-xs-12 col-sm-11 col-md-10 col-lg-9">
             <? $a = new Area('Text Left');$a->display($c); ?>
         </div>
         <div class="clearfix"></div>
