28ca5095b5531688d1b635f9cebf1b9e52f4ab60
Robert Evans <robert@tresnadesigns.com>
2015-06-03 12:40:33 -0700

    Legal page changes

--- application/themes/af/legal.php
+++ application/themes/af/legal.php
@@ -1,11 +1,15 @@
 <?php 
 $view->inc('elements/header.php');  ?>
-<section class="top-section">
-</section>
 
-<div class="top-section">
-</section>
-    <? $a = new Area('Section Main Area');$a->display($c); ?>
+<div class="container top-section">
+    <div class="row">
+	<div class="col-md-2"><? $a = new Area('Legal Sections Nav');$a->display($c); ?></div>
+    <div class="col-md-10"><? $a = new Area('Section Main Area');$a->display($c); ?></div>
+    </div>
+</div>
+
+<div class="marginbot40"></div>
+<div class="separator saperator-gradient"></div>
 
  <?php $view->inc('elements/footer.php'); ?>
 <? $a = new Area('php script');$a->display($c); ?>
