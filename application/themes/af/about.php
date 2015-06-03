3bf08c87dc0ee6bfababf7d03b492a0fb2110729
Robert Evans <robert@tresnadesigns.com>
2015-06-02 19:52:41 -0700

    testing again

--- application/themes/af/about.php
+++ application/themes/af/about.php
@@ -57,7 +57,13 @@ $view->inc('elements/header.php');  ?>
 </div>
 
 <section class="section section-join-team">
-    <?php $view->inc('elements/joinaf-text-left-img-right.php'); ?>
+    <div class="leftfadeblue backstretch-contain">
+    <div class="container">
+        <div class="col-md-6 align-left"><? $a = new GlobalArea('Join Text Left');$a->display($c); ?></div>
+        <div class="col-md-6 align-right"><? $a = new GlobalArea('Join Text Right');$a->display($c); ?></div>
+        <div class="clearfix"></div>
+    </div>
+	</div>
 </section>
 
     
