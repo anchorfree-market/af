ed4b3ac09ff8dd647b7289474d286da4e9af58c7
Robert Evans <robert@tresnadesigns.com>
2015-06-02 18:27:24 -0700

    changes again

--- application/themes/af/about.php
+++ application/themes/af/about.php
@@ -7,49 +7,42 @@ $view->inc('elements/header.php');  ?>
 <div class="wrapper mid about">
     <div class="container">
         <div class="col-md-12">
-            <h2 class="main-title align-center"><? $a = new Area('About Title');$a->display($c);?></h2>
-            <p class="marginbot50><? $a = new Area('About Content');$a->display($c);?></p>
+            <h2 class="main-title align-center-imp"><? $a = new Area('About Title');$a->display($c);?></h2>
+            <p class="marginbot50 align-center-imp"><? $a = new Area('About Content');$a->display($c);?></p>
         </div>
+        
         <div class=" col-md-12 separator saperator-gradient marginbot40"></div>
-        <div class="col-md-12">
-            <h2>Contact AnchorFree Inc.</h2>
-        </div>
-        <div class="col-md-6 col-sm-6 address">
-            <h4>US. Headquarters</h4>
-            <? $a = new Area('US Headquarter Address');$a->display($c);?>
-        </div>
-        <div class="col-md-6 col-sm-6 address">
-            <h4>Swiss Location</h4>
-            <? $a = new Area('Swiss Address');$a->display($c);?>
-        </div>
-    </div>
-</div>
-<section class="map">
-    <? $a = new Area('Map');$a->display($c);?>
-</section>
+        <div class="col-md-12"><h2><? $a = new Area('Address Title');$a->display($c);?></h2></div>
+        <div class="row">
+        <div class="col-md-2"><h4><? $a = new Area('US Headquarter Address Title');$a->display($c);?></h4></div>
+        <div class="col-md-2"><h4><? $a = new Area('US Headquarter Address');$a->display($c);?></h4></div>
+        <div class="col-md-2"><h4><? $a = new Area('Swiss Address Title');$a->display($c);?></h4></div>
+        <div class="col-md-2"><h4><? $a = new Area('Swiss Address');$a->display($c);?></h4></div>
+    	</div>
+	</div>
+
+<div class="map"><? $a = new Area('Map');$a->display($c);?></div>
+
 <div class="wrapper fullfadeblue">
     <div class="container">
-        <h2><? $a = new Area('Form Title');$a->display($c);?></h2>
+        <h2 class="align-left-imp"><? $a = new Area('Form Title');$a->display($c);?></h2>
         <div class="col-md-12"><? $a = new Area('Form');$a->display($c);?></div>
     </div>
 </div>
 <div class="wrapper meet-visionaries">
     <div class="container">
-        <h2>Meet the Visionaries</h2>
-            <div class="col-md-4 col-sm-4 col-xs-6"><? $a = new Area('vision-image-1');$a->display($c);?><? $a = new Area('vision-desc-1');$a->display($c);?></div>
-            <div class="col-md-4 col-sm-4 col-xs-6"><? $a = new Area('vision-image-2');$a->display($c);?><? $a = new Area('vision-desc-2');$a->display($c);?></div>
-            <div class="col-md-4 col-sm-4 col-xs-6"><? $a = new Area('vision-image-3');$a->display($c);?><? $a = new Area('vision-desc-3');$a->display($c);?></div>
-
-
-            <div class="col-md-4 col-sm-4 col-xs-6"><? $a = new Area('vision-image-4');$a->display($c);?><? $a = new Area('vision-desc-4');$a->display($c);?></div>
-            <div class="col-md-4 col-sm-4 col-xs-6"><? $a = new Area('vision-image-5');$a->display($c);?><? $a = new Area('vision-desc-5');$a->display($c);?></div>
-            <div class="col-md-4 col-sm-4 col-xs-6"><? $a = new Area('vision-image-6');$a->display($c);?><? $a = new Area('vision-desc-6');$a->display($c);?></div>
-
-    </div>
+        <div class="col-md-12"><h2><? $a = new Area('Team Title');$a->display($c);?></h2></div>
+        <div class="col-md-4 col-sm-4 col-xs-6"><? $a = new Area('vision-image-1');$a->display($c);?><? $a = new Area('vision-desc-1');$a->display($c);?></div>
+        <div class="col-md-4 col-sm-4 col-xs-6"><? $a = new Area('vision-image-2');$a->display($c);?><? $a = new Area('vision-desc-2');$a->display($c);?></div>
+        <div class="col-md-4 col-sm-4 col-xs-6"><? $a = new Area('vision-image-3');$a->display($c);?><? $a = new Area('vision-desc-3');$a->display($c);?></div>
+        <div class="col-md-4 col-sm-4 col-xs-6"><? $a = new Area('vision-image-4');$a->display($c);?><? $a = new Area('vision-desc-4');$a->display($c);?></div>
+        <div class="col-md-4 col-sm-4 col-xs-6"><? $a = new Area('vision-image-5');$a->display($c);?><? $a = new Area('vision-desc-5');$a->display($c);?></div>
+        <div class="col-md-4 col-sm-4 col-xs-6"><? $a = new Area('vision-image-6');$a->display($c);?><? $a = new Area('vision-desc-6');$a->display($c);?></div>
+	</div>
 </div>
 <div class="wrapper investors-board-advisors">
     <div class="container">
-       <h2>Investors &amp; Board of Advisors</h2>
+       <div class="col-md-12"><h2><? $a = new Area('Investors Title');$a->display($c);?></h2></div>
        <div class="col-md-4 col-sm-4"><? $a = new Area('Investors Board Advisors Column 1');$a->display($c);?></div>
        <div class="col-md-4 col-sm-4"><? $a = new Area('Investors Board Advisors Column 2');$a->display($c);?></div>
        <div class="col-md-4 col-sm-4"><? $a = new Area('Investors Board Advisors Column 3');$a->display($c);?></div>
@@ -57,9 +50,9 @@ $view->inc('elements/header.php');  ?>
     
     
 </div>
-<section class="section section-join-team join-team">
+<div class="wrapper col-md-12">
     <?php $view->inc('elements/joinaf-text-left-img-right.php'); ?>
-</section>
+</div>
     
 <?php $view->inc('elements/footer.php'); ?>
 <? $a = new Area('Backstretch Script');$a->display($c); ?>
