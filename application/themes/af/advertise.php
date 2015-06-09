e63d0c4a8cece079e73e144270eed1a91421a02f
Robert Evans <robert@tresnadesigns.com>
2015-06-09 14:38:26 -0700

    advertise change

--- application/themes/af/advertise.php
+++ application/themes/af/advertise.php
@@ -5,7 +5,7 @@ $view->inc('elements/header.php');  ?>
     $GLOBALS['topSecondEle'] = 'h3';
 ?>
 
-<section class="section fullfadeblue">
+<section class="section section-featured fullfadeblue">
     <div class="wrapper">
         <div class="container">
             <div class="row">
