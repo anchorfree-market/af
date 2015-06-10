0adbd34b3b0bc086f596ab3b1040e9e9712b6c73
Robert Evans <robert@tresnadesigns.com>
2015-06-09 20:54:35 -0700

    pull

--- application/themes/af/elements/txt-right-img-left.php
+++ application/themes/af/elements/txt-right-img-left.php
@@ -1,7 +1,7 @@
 <div class="rightfadeblue backstretch-contain">
     <div class="container">
         <div class="col-md-6 align-left-imp"><? $a = new Area('Text Left');$a->display($c); ?></div>
-        <div class="col-md-6 col-xs-8 align-right-imp"><? $a = new Area('Text Right');$a->display($c); ?></div>
+        <div class="col-md-6 col-xs-8 align-right-imp pull-right"><? $a = new Area('Text Right');$a->display($c); ?></div>
         <div class="clearfix"></div>
     </div>
 </div>
