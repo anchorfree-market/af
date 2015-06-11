fd3ed00aa366927a26a91a4758094acee1d1fea8
Robert Evans <robert@tresnadesigns.com>
2015-06-10 17:33:51 -0700

    css changes

--- application/themes/af/legal.php
+++ application/themes/af/legal.php
@@ -3,8 +3,8 @@ $view->inc('elements/header.php');  ?>
 
 <div class="container top-section">
     <div class="row top-section">
-	<div class="col-md-2"><? $a = new Area('Legal Sections Nav');$a->display($c); ?></div>
-    <div class="col-md-10"><? $a = new Area('Section Main Area');$a->display($c); ?></div>
+	<div class="col-md-2 col-sm-2 hidden-xs"><? $a = new Area('Legal Sections Nav');$a->display($c); ?></div>
+    <div class="col-md-10 col-sm-10 col-xs-12"><? $a = new Area('Section Main Area');$a->display($c); ?></div>
     </div>
 </div>
 
