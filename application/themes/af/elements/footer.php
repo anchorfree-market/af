11230c2fe605c1e7a4c500f02d5b5cd7e6c41ff3
Esther Lau <esther@anchorfree.com>
2015-07-24 14:23:09 -0700

    moved jQuery and backstretch js to header

--- application/themes/af/elements/footer.php
+++ application/themes/af/elements/footer.php
@@ -14,7 +14,6 @@
                 </div>
         </footer>
 </div>
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <!-- only show bootstrap.min.js if isEditMode -->
     <?php $c = Page::getCurrentPage();
         if($c->isEditMode()) {} 
@@ -27,7 +26,6 @@
         $themePath = $view->getThemePath();
         if(preg_match("/carriers/", $URL)) { // display script on carriers page
             echo "<script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js'></script>";
-            echo "<script type='text/javascript' src='".$themePath."/js/jquery.backstretch.min-ck.js'></script>";
             echo "<script type='text/javascript' src='".$themePath."/js/carriers.js'></script>";
             echo "<script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/js/jasny-bootstrap.min.js'></script>";
         }
@@ -39,9 +37,7 @@
         else if(preg_match("/careers/", $URL) || preg_match("/jobs/", $URL)) {    // display script on careers page
             echo "<script src='//code.jquery.com/ui/1.11.4/jquery-ui.js'></script>";
         }
-        else { // including press and about page
-            echo "<script type='text/javascript' src='".$themePath."/js/jquery.backstretch.min-ck.js'></script>";
-        }
+        else { // including press and about page}
     ?>
     </div>
     <? Loader::element('footer_required')?>
