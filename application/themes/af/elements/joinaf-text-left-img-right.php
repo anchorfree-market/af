39dd514aa50bedff5707f20072e569b516cffcfb
estherylau <yelinglau@gmail.com>
2015-10-01 16:41:04 -0700

    fixed joinaf-text-left-img-right area; edited dropdown menu a tag style;

--- application/themes/af/elements/joinaf-text-left-img-right.php
+++ application/themes/af/elements/joinaf-text-left-img-right.php
@@ -1,7 +1,7 @@
 <div class="backstretch-contain leftfadeblue">
     <div class="container">
         <div class="col-md-6 col-sm-8 col-xs-8 align-left-imp"><? $a = new GlobalArea('Join Text Left');$a->display($c); ?></div>
-        <div class="col-md-6 align-right-imp"><? $a = new GlobalArea('Join Text Right');$a->display($c); ?></div>
+        <div class="col-md-6 align-right-imp"><? $a = new GlobalArea('Join Txt Right');$a->display($c); ?></div>
         <div class="clearfix"></div>
     </div>
 </div>
