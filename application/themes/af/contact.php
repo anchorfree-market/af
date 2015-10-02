c3f197d8af5c04e5d9a5d0d1fe3dcb9bc29def04
estherylau <yelinglau@gmail.com>
2015-10-02 11:30:54 -0700

    added stacks in contact; edited stylesheet;

--- application/themes/af/contact.php
+++ application/themes/af/contact.php
@@ -3,7 +3,7 @@ $view->inc('elements/header.php');  ?>
 <div class="wrapper">
     <div class="container">
         <div class="col-md-12 marginbot15"><h2><? $a = new Area('Address Title');$a->display($c);?></h2></div>
-        <div class="row">
+        <div class="row contact-address">
             <div class="col-md-2 col-md-offset-1 col-xs-12"><h4><? $a = new Area('US Headquarter Address Title');$a->display($c);?></h4></div>
             <div class="col-md-3 col-xs-12"><h4 class="align-left-imp"><? $a = new Area('US Headquarter Address');$a->display($c);?></h4></div>
             <div class="col-md-2 col-md-offset-1 col-xs-12"><h4><? $a = new Area('Swiss Address Title');$a->display($c);?></h4></div>
@@ -12,33 +12,23 @@ $view->inc('elements/header.php');  ?>
         <div class="row contact-info">
             <div class="col-md-4">
                 <div class="contact-block">
-                    <img src="<?php echo $view->getThemePath()?>/images/icn_user_bug@2x.png" />
-                    <h5>For Advertising Inquiries</h5>
-                    <a href="mailto:advertise@anchorfree.com">advertise@anchorfree.com</a>
+                    <? $a = new Area('Contact Upper Left');$a->display($c);?>
                 </div>
                 <div class="contact-block">
-                    <img src="<?php echo $view->getThemePath()?>/images/icn_user_bug@2x.png" />
-                    <h5>For Advertising Inquiries</h5>
-                    <a href="mailto:advertise@anchorfree.com">advertise@anchorfree.com</a>
+                    <? $a = new Area('Contact Lower Left');$a->display($c);?>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="contact-block">
-                    <img src="<?php echo $view->getThemePath()?>/images/icn_user_bug@2x.png" />
-                    <h5>For Advertising Inquiries</h5>
-                    <a href="mailto:advertise@anchorfree.com">advertise@anchorfree.com</a>
+                    <? $a = new Area('Contact Center');$a->display($c);?>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="contact-block">
-                    <img src="<?php echo $view->getThemePath()?>/images/icn_user_bug@2x.png" />
-                    <h5>For Advertising Inquiries</h5>
-                    <a href="mailto:advertise@anchorfree.com">advertise@anchorfree.com</a>
+                    <? $a = new Area('Contact Upper Right');$a->display($c);?>
                 </div>
                 <div class="contact-block">
-                    <img src="<?php echo $view->getThemePath()?>/images/icn_user_bug@2x.png" />
-                    <h5>For Advertising Inquiries</h5>
-                    <a href="mailto:advertise@anchorfree.com">advertise@anchorfree.com</a>
+                    <? $a = new Area('Contact Lower Right');$a->display($c);?>
                 </div>
 
             </div>
