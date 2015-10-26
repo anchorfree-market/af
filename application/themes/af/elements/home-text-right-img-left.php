b0b88ecd08f42ca2fe88e12e637c778fdaf7bb73
estherylau <yelinglau@gmail.com>
2015-10-26 14:01:32 -0700

    changed button size

--- application/themes/af/elements/home-text-right-img-left.php
+++ application/themes/af/elements/home-text-right-img-left.php
@@ -1,7 +1,7 @@
 <div class="backstretch-contain leftfadewhite">
     <div class="container">
         <div class="col-md-6 align-left-imp"><? $a = new Area('Product Text Left');$a->display($c); ?></div>
-        <div class="col-md-6 col-sm-8 col-xs-10 align-right-imp pull-right"><? $a = new Area('Product Text Right');$a->display($c); ?></div>
+        <div class="col-md-6 col-sm-10 col-xs-11 align-right-imp pull-right"><? $a = new Area('Product Text Right');$a->display($c); ?></div>
         <div class="clearfix"></div>
     </div>
 </div>
