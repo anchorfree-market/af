21c3424b361e697e2220a6d80f14bfc414af8fd2
estherylau <yelinglau@gmail.com>
2015-11-11 10:36:56 -0800

    changed css; added ie8 responsive support script

--- application/themes/af/elements/header.php
+++ application/themes/af/elements/header.php
@@ -18,6 +18,8 @@
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script type='text/javascript' src='<?php echo $view->getThemePath()?>/js/jquery.backstretch.min-ck.js'></script>
     <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <link rel="stylesheet" href="<?php echo $view->getThemePath()?>/css/ie.css"/>
     <![endif]-->
 </head>
